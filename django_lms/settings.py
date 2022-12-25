60f2d5e6582889b9a59835329800abd98f5ed5da
MannAmore <mandaistiantoko@gmail.com>
2022-12-25 17:54:28 +0800

    db config

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -78,10 +78,26 @@ WSGI_APPLICATION = 'django_lms.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'G3RBpnyoCHFT6jnIEjQ9',
+        'HOST': 'containers-us-west-24.railway.app',
+        'PORT': '6194'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'my_app',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
