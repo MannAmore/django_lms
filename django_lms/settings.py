127acbe05f7f143c6bf4a45616fa5a933abe4199
MannAmore <mandaistiantoko@gmail.com>
2022-12-25 21:34:56 +0800

    new

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -46,7 +46,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': ['./lmsApp/templates/'],
-        'DIRS': ['./django_lms/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
