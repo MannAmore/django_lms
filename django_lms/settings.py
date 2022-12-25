deaff3c421e134abec30a8b9b39953c7cc6d82c4
MannAmore <mandaistiantoko@gmail.com>
2022-12-25 18:51:40 +0800

    pleasee

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -46,6 +46,7 @@ ROOT_URLCONF = 'django_lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
